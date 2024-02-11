e69ffd83d22f401846b3347d43e0ed61f76a0fdd
bxnkalert <149743268+bxnkalert@users.noreply.github.com>
2024-02-11 16:10:55 +0700

    [Added] Map in Store and Process

---
+++ src/data/review.js
@@ -0,0 +1,27 @@
+var review = [
+    {
+      "province": 39,
+      "user": "Thong-natee Ice factory",
+      "review": "IFX has revolutionized the way we manage our ice factory. The platform's seamless integration of customers, sales, production, delivery, and accounting has made our operations more efficient and profitable. We can increase our revenue more than 20%</b> in the first month. Thank you IFX for your excellence in service!"
+    },
+    {
+      "province": 39,
+      "user": "Thong-natee Ice factory",
+      "review": "IFX has revolutionized the way we manage our ice factory. The platform's seamless integration of customers, sales, production, delivery, and accounting has made our operations more efficient and profitable. We can increase our revenue more than 20%</b> in the first month. Thank you IFX for your excellence in service!"
+    },
+    {
+      "province": 58,
+      "user": "Somsong",
+      "review": "Good"
+    },
+    {
+      "province": 68,
+      "user": "Band",
+      "review": "Medium"
+    },
+    {
+      "province": 69,
+      "user": "Phonl",
+      "review": "Bad"
+    }
+  ];
