df0e3b473f6cd4d8ed138ace87415cdc0e9abca0
bxnkalert <149743268+bxnkalert@users.noreply.github.com>
2024-02-12 09:29:26 +0700

    [Edited & Added] Font Support Thai Language and Add thai word.

---
+++ language/language-data.js
@@ -0,0 +1,477 @@
+var language = [
+    {
+        "id": "menu-1",
+        "th": "หน้าหลัก",
+        "en": "Home"
+    },
+    {
+        "id": "menu-2",
+        "th": "เคยหรือไม่ ?",
+        "en": "Have you ever ?"
+    },
+    {
+        "id": "menu-3",
+        "th": "เกี่ยวกับเรา",
+        "en": "About Us"
+    },
+    {
+        "id": "menu-4",
+        "th": "ขั้นตอน กระบวนการ",
+        "en": "Process"
+    },
+    {
+        "id": "menu-5",
+        "th": "แพลตฟอร์มของเรา",
+        "en": "Ours Platform"
+    },
+    {
+        "id": "menu-6",
+        "th": "ปัญหา ความต้องการของลูกค้า",
+        "en": "Customer Pain"
+    },
+    {
+        "id": "menu-7",
+        "th": "สาขา",
+        "en": "Stores"
+    },
+    {
+        "id": "menu-8",
+        "th": "แพคเกจ และเซอร์วิส",
+        "en": "Package & Service"
+    },
+    {
+        "id": "menu-9",
+        "th": "ติดต่อเรา",
+        "en": "Contact Us"
+    },
+    {
+        "id": "menu-privacy",
+        "th": "ความเป็นส่วนตัว",
+        "en": "Privacy"
+    },
+    {
+        "id": "menu-login",
+        "th": "เข้าสู่ระบบ",
+        "en": "Login"
+    },
+    {
+        "id": "menu-mobile-1",
+        "th": "หน้าหลัก",
+        "en": "Home"
+    },
+    {
+        "id": "menu-mobile-2",
+        "th": "เคยหรือไม่ ?",
+        "en": "Have you ever ?"
+    },
+    {
+        "id": "menu-mobile-3",
+        "th": "เกี่ยวกับเรา",
+        "en": "About Us"
+    },
+    {
+        "id": "menu-mobile-4",
+        "th": "ขั้นตอน กระบวนการ",
+        "en": "Process"
+    },
+    {
+        "id": "menu-mobile-5",
+        "th": "แพลตฟอร์มของเรา",
+        "en": "Ours Platform"
+    },
+    {
+        "id": "menu-mobile-6",
+        "th": "ปัญหา ความต้องการของลูกค้า",
+        "en": "Customer Pain"
+    },
+    {
+        "id": "menu-mobile-7",
+        "th": "สาขา",
+        "en": "Stores"
+    },
+    {
+        "id": "menu-mobile-8",
+        "th": "แพคเกจ และเซอร์วิส",
+        "en": "Package & Service"
+    },
+    {
+        "id": "menu-mobile-9",
+        "th": "ติดต่อเรา",
+        "en": "Contact Us"
+    },
+    {
+        "id": "intro-header",
+        "th": "เพิ่มประสิทธิภาพการจัดการโรงงานน้ำแข็งของคุณ",
+        "en": "Streamline your Ice Factory Operations"
+    },
+    {
+        "id": "intro-description",
+        "th": "จัดการลูกค้า การขาย การผลิต การจัดส่ง และการบัญชีอย่างมีประสิทธิภาพทั้งหมดในแพลตฟอร์มเดียว",
+        "en": "Efficiently manage customer, sales, production, delivery and accounting all in One platform"
+    },
+    {
+        "id": "problem-header",
+        "th": "พบปัญหาเหล่านี้อยู่หรือไม่ ?",
+        "en": "Have you ever encountered these problems ?"
+    },
+    {
+        "id": "problem-owner",
+        "th": "เจ้าของโรงงาน",
+        "en": "Owner"
+    },
+    {
+        "id": "problem-customer",
+        "th": "ลูกค้า",
+        "en": "Customer"
+    },
+    {
+        "id": "problem-delivery",
+        "th": "ฝ่ายขนส่ง",
+        "en": "Delivery"
+    },
+    {
+        "id": "problem-1",
+        "th": "- ต้องอยู่ประจำ ทิ้งโรงงานไม่ได้",
+        "en": "- Must stay permanently, cannot abandon the factory."
+    },
+    {
+        "id": "problem-2",
+        "th": "- ถังสูญหาย ซื้อเพิ่มทุกปี",
+        "en": "- Tanks go missing, have to buy new ones every year."
+    },
+    {
+        "id": "problem-3",
+        "th": "- บอกไม่ได้ว่าร้านที่ลงถัง ยอดขายเป็นยังไง",
+        "en": "- Can't say how the sales are for the shops with the tanks."
+    },
+    {
+        "id": "problem-4",
+        "th": "- ประเมินผลพนักงานยาก",
+        "en": "- Difficult to evaluate employee performance."
+    },
+    {
+        "id": "problem-5",
+        "th": "- อำนาจต่อรองน้อย ต้องง้อ",
+        "en": "- Limited negotiation power, have to yield."
+    },
+    {
+        "id": "problem-6",
+        "th": "- ไม่ได้รับสินค้า",
+        "en": "- Didn't receive the goods."
+    },
+    {
+        "id": "problem-7",
+        "th": "- ส่งไม่ตรงเวลา",
+        "en": "- Delivery not on time."
+    },
+    {
+        "id": "problem-8",
+        "th": "- เป็นลูกค้ามานาน ไม่มีโปรโมชัน ของแถม",
+        "en": "- Been a customer for a long time, no promotions or free items."
+    },
+    {
+        "id": "problem-9",
+        "th": "- ส่งไม่ตรงเวลา ลูกค้า COMPLAINT",
+        "en": "- Late delivery, customer complaints."
+    },
+    {
+        "id": "problem-10",
+        "th": "- รถวิ่งทับเส้นทางกันเอง ยอดขายไม่เพิ่ม",
+        "en": "- Vehicles overlapping routes, sales not increasing."
+    },
+    {
+        "id": "problem-11",
+        "th": "- คนขับใหม่ ไม่รู้เส้นทาง ไม่รู้ตำแหน่งร้าน",
+        "en": "- New drivers, unaware of routes and shop locations."
+    },
+    {
+        "id": "about-us-header",
+        "th": "เกี่ยวกับเรา",
+        "en": "About US"
+    },
+    {
+        "id": "about-us-description",
+        "th": "IFX (Ice Factory Excellence) คือ แพลตฟอร์มสำหรับบริหารโรงงานน้ำแข็งแบบครบวงจร ตั้งแต่กระบวนการขาย การผลิต การจัดส่งคลังสินค้าและบัญชี ที่ถูกพัฒนาและจดลิขสิทธิ์โดยบริษัท อินเวสฮับ จำกัด บริษัทฯที่เป็นแหล่งรวมผู้เชี่ยวชาญที่มีประสบการณ์ด้านการออกแบบ และพัฒนาระบบ เพื่อแก้ปัญหาให้กับองค์กรชั้นนำในประเทศไทยมามากกว่า 15 ปี โดยบริษัทฯมีจุดมุ่งหมายคือนําระบบไอทีเข้ามาช่วยเหลือผู้ประกอบการให้สามารถบริหารธุรกิจและเติบโตได้อย่างยั่งยืน",
+        "en": "At Inveshub, we provide a comprehensive platform to manage your ice factory. Our platform (IFX) includes customer, sales, production, delivery and accounting. All in one place. We understand the challenges of running an ice factory, and we're here to help."
+    },
+    {
+        "id": "process-owner",
+        "th": "เจ้าของโรงงาน",
+        "en": "Owner"
+    },
+    {
+        "id": "process-customer",
+        "th": "ลูกค้า หรือร้านค้า",
+        "en": "Customer / Stores"
+    },
+    {
+        "id": "process-sales",
+        "th": "ฝ่ายขาย",
+        "en": "Sales"
+    },
+    {
+        "id": "process-production",
+        "th": "ฝ่ายผลิต",
+        "en": "Production"
+    },
+    {
+        "id": "process-delivery",
+        "th": "ฝ่ายขนส่ง",
+        "en": "Delivery"
+    },
+    {
+        "id": "process-accounting",
+        "th": "บัญชี และธุรการ",
+        "en": "Accounting & Administrative"
+    },
+    {
+        "id": "process-1",
+        "th": "- รายงานผลดำเนินการประจำวันผ่านทาง Line",
+        "en": "- Daily operation reports via Line."
+    },
+    {
+        "id": "process-2",
+        "th": "- ระบบที่อุดรอยรั่วขององค์กร ปิดช่องโหว่งการทุจริต และสามารถตรวจสอบข้อมูลได้ทุกมิติตลอดทั้งกระบวนการ",
+        "en": "- The organization's vulnerability-sealing system closes gaps in corruption and allows for comprehensive data monitoring throughout the process."
+    },
+    {
+        "id": "process-3",
+        "th": "- บันทึกตรวจสอบข้อมูลร้านค้า และถัง",
+        "en": "- Record inspections of store and tank data."
+    },
+    {
+        "id": "process-4",
+        "th": "- ตรวจสอบประวัติการสั่งซื้อสินค้า และยอดการซื้อรวมและจัดการโปรโมชัน",
+        "en": "- Check the history of product orders, total purchase amounts, and manage promotions."
+    },
+    {
+        "id": "process-5",
+        "th": "- รองรับการชำระเงินสด เครดิต QR Prompt pay",
+        "en": "- Support cash, credit, QR, and Prompt Pay payment."
+    },
+    {
+        "id": "process-6",
+        "th": "- สามารถบริหารจัดการคำสั่งซื้อ และแผนการจัดส่งอย่างมีประสิทธิภาพ",
+        "en": "- Efficiently manage orders and delivery plans."
+    },
+    {
+        "id": "process-7",
+        "th": "- รองรับการสั่งซื้อสินค้าด่วน มั่นใจว่าไม่มีรายการไหนหลุด",
+        "en": "- Support urgent product orders, ensuring no orders are missed."
+    },
+    {
+        "id": "process-8",
+        "th": "- สามารถออกโปรโมชั่นที่ตอบโจทย์",
+        "en": "- Able to create promotions that meet the needs."
+    },
+    {
+        "id": "process-9",
+        "th": "- ตรวจสอบข้อมูลการซื้อสินค้าย้อนหลัง และสร้างโอกาสในการขยายตลาด",
+        "en": "- Review historical product purchase data and create opportunities for market expansion."
+    },
+    {
+        "id": "process-10",
+        "th": "- ตรวจสอบการวางแผนผลิต และผลที่ได้จริง",
+        "en": "- Check production planning and actual results."
+    },
+    {
+        "id": "process-11",
+        "th": "- บริหารจัดการสินค้าในคลัง และลดปัญหา stock ล้น",
+        "en": "- Manage warehouse products and reduce stock overflow issues."
+    },
+    {
+        "id": "process-12",
+        "th": "- ตรวจสอบสถานะคนขับ",
+        "en": "- Check the status of drivers."
+    },
+    {
+        "id": "process-13",
+        "th": "- นำทางและคำนวณยอดเงินแบบ Real time",
+        "en": "- Navigate and calculate real-time financial figures."
+    },
+    {
+        "id": "process-14",
+        "th": "- ตรวจสอบระยะทางวิ่งเพื่อประเมินศักยภาพของตลาดและคนขับ",
+        "en": "- Check running distances to assess market and driver potential."
+    },
+    {
+        "id": "process-15",
+        "th": "- ช่วยสรุปยอดเงินและบันทึกรายการใช้จ่าย",
+        "en": "- Summarize financial figures and record expenses."
+    },
+    {
+        "id": "process-16",
+        "th": "- ตรวจสอบสินค้าในคลัง และในรถแบบ Realtime",
+        "en": "- Check warehouse and in-vehicle products in real-time."
+    },
+    {
+        "id": "process-17",
+        "th": "- ตรวจสอบการผลิตและผลผลิต",
+        "en": "- Check production processes and results."
+    },
+    {
+        "id": "process-18",
+        "th": "- ตรวจสอบถังน้ำแข็งและถุง",
+        "en": "- Inspect ice chest and bags."
+    },
+    {
+        "id": "process-19",
+        "th": "- ตรวจสอบยอดขายรายร้านค้า ยอดเงินสด การใช้งานโปรโมชัน การขายนอกแผน ยอดเครดิต ยอดค้างชำระ",
+        "en": "- Check store-wise sales, cash amounts, promotion usage, off-plan sales, credit amounts, and outstanding balances."
+    },
+    {
+        "id": "process-20",
+        "th": "- ตรวจสอบยอดรายจ่ายของคนขับ",
+        "en": "- Check driver expenses."
+    },
+    {
+        "id": "platform-header",
+        "th": "แพลตฟอร์มของเรา",
+        "en": "Ours Platform"
+    },
+    {
+        "id": "platform-customer",
+        "th": "ลูกค้า",
+        "en": "Customers"
+    },
+    {
+        "id": "platform-production-delivery",
+        "th": "ฝ่ายผลิต และฝ่ายจัดส่ง",
+        "en": "Production - Delivery"
+    },
+    {
+        "id": "platform-sales-accounting",
+        "th": "ฝ่ายขาย และฝ่ายบัญขี ธุรการ",
+        "en": "Sales - Accounting"
+    },
+    {
+        "id": "platform-customer-description",
+        "th": "จัดเก็บ และใช้ข้อมูลลูกค้าของคุณ เพื่อให้พวกเขาพึงพอใจกับบริการของคุณบนแพลตฟอร์มของเรา",
+        "en": "Keep and utilize your customer information to keep them satisfy with your service on our platform."
+    },
+    {
+        "id": "platform-production-delivery-description",
+        "th": "จัดการ และควบคุมการผลิตของคุณ ตามแผนการจัดส่ง และตรวจสอบพนักขับรถของคุณในเวลาจริง เพื่อลดเวลาการขนส่ง และต้นทุนการดำเนินงาน",
+        "en": "Manage and control your production according to your delivery plan and monitor your driver in real-time. Reduce transportation time and operation cost."
+    },
+    {
+        "id": "platform-sales-accounting-description",
+        "th": "ตรวจสอบประสิทธิภาพขาย การผลิตที่มีประสิทธิภาพ สถานะคลังสินค้า และติดตามค่าใช้จ่าย ทั้งหมดในที่เดียว",
+        "en": "Monitor sales performance. production efficient, warehouse status and track expenses. All in one place."
+    },
+    {
+        "id": "customer-pain-problem",
+        "th": "ปัญหา ความต้องการของลูกค้า",
+        "en": "Customer Pain & Problem"
+    },
+    {
+        "id": "stores-header",
+        "th": "สาขา",
+        "en": "Stores"
+    },
+    {
+        "id": "stores-review",
+        "th": "รีวิวจากผู้ใช้งาน",
+        "en": "Review"
+    },
+    {
+        "id": "package-header",
+        "th": "แพคเกจ และเซอร์วิส",
+        "en": "Packages & Service"
+    },
+    {
+        "id": "package-table-package",
+        "th": "แพคเกจ",
+        "en": "Packages"
+    },
+    {
+        "id": "package-table-drop-point",
+        "th": "จุดบริการ",
+        "en": "Drop Point"
+    },
+    {
+        "id": "package-table-car",
+        "th": "จำนวนรถ",
+        "en": "Car"
+    },
+    {
+        "id": "package-table-price",
+        "th": "ราคา (บาท)",
+        "en": "Amount (THB)"
+    },
+    {
+        "id": "package-table-day",
+        "th": "จำนวน (วัน)",
+        "en": "Amount (Day)"
+    },
+    {
+        "id": "package-service-header",
+        "th": "ขอบเขตการให้บริการ",
+        "en": "Service Scope"
+    },
+    {
+        "id": "package-pricing-description-1",
+        "th": "การคำนวณราคา จะคำนวณจากลูกค้าที่ใช้บริการ (drop-point) ในระบบ โดยค่าใช้จ่ายจะสรุปทุกวันที่ 1 โดยคำนวณจากจำนวนลูกค้าที่ใช้บริการ (drop-point) มากที่สุดในเดือนก่อนหน้านั้น ค่าบริการจะถูกหักโดยอัตโนมัติจากบัตรเครดิตของคุณทุกวันที่ 16 ของทุกเดือน",
+        "en": "Pricing is calculated based on the active customer(drop-point) in the system. The cost is summarized monthly on the 1st day, using the maximum number of active customer(drop-point) in that previous month. Service fee will automatically deduct from your credit-card on every 16th of month."
+    },
+    {
+        "id": "package-pricing-description-2",
+        "th": "ตัวอย่างเช่น; โรงงาน A มีลูกค้าที่ใช้บริการมากที่สุดในเดือนกรกฎาคม 100 drop-point เมื่อถึงวันที่ 1 สิงหาคม ค่าบริการจะถูกคำนวณดังนี้: จำนวนลูกค้าที่ใช้บริการ * ราคาแพ็คเกจ * จำนวนวัน * VAT 7% * ค่าบริการ 3.65% = ค่าบริการ 100 * 0.38 บาท * 31 * 1.07 * 1.0365 = 1,306.46 บาท",
+        "en": "For example; Factory A has maximum active customers in July 100 drop-point. 1st August, the service fee will be calculated as below. active customer * package price * day * VAT7%* Service charge 3.65% = service fee 100*0.38THB*31*1.07*1.0365 = 1,306.46 THB"
+    },
+    {
+        "id": "package-service-1",
+        "th": "ระบบสามารถใช้งานได้ตลอด 24 ชม. โดยมี Uptime warranty ที่ 99.5%",
+        "en": "The platform is available for use and supports 24/7 operations with a 99.5% Up-time Warranty."
+    },
+    {
+        "id": "package-service-2",
+        "th": "ทีมงานที่ให้คำปรึกษา สอนการใช้งาน และแก้ไขปัญหาตลอดระยะเวลาที่ใช้งานระบบ โดยมีช่วงการให้บริการตั้งแต่ 8 โมงถึง 5 โมงเย็นทุกวันจันทร์-ศุกร์",
+        "en": "Support teams are available for assistance, consultation, and user training on regular business days from 8:00 AM to 5:00 PM."
+    },
+    {
+        "id": "package-service-3",
+        "th": "ทีมพัฒนาระบบพร้อมรับฟังความต้องการใหม่ๆ จากลูกค้าเพื่อนำมาพัฒนาระบบ",
+        "en": "The development team works to improve efficiency and introduce new functionalities to meet user requirements."
+    },
+    {
+        "id": "package-service-4",
+        "th": "และมีการสำรองข้อมูล รายวันและรายเดือนเพื่อให้ลูกค้ามั่นใจ และป้องกันข้อมูลสูญหายอีกด้วย",
+        "en": "Daily and monthly data backups are performed to ensure data redundancy."
+    },
+    {
+        "id": "contact-us-header",
+        "th": "ติดต่อเรา",
+        "en": "Contact Us"
+    },
+    {
+        "id": "contact-us-name",
+        "th": "ชื่อ - นามสกุล",
+        "en": "FirstName - Surname"
+    },
+    {
+        "id": "contact-us-tel",
+        "th": "เบอร์ติดต่อ",
+        "en": "Tel"
+    },
+    {
+        "id": "contact-us-email",
+        "th": "อีเมล",
+        "en": "E-mail"
+    },
+    {
+        "id": "contact-us-message",
+        "th": "ข้อความ",
+        "en": "Message"
+    },
+    {
+        "id": "contact-us-submit",
+        "th": "ส่งข้อมูล",
+        "en": "Submit"
+    },
+    {
+        "id": "information-footer",
+        "th": "ช่องทางการติดต่อ",
+        "en": "information"
+    },
+];
